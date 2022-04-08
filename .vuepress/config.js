a77eb7c18fbd32a3c7e4c579c229928de58b4600
xiol-liang <2318751031@qq.com>
2022-04-08 17:59:51 +0800

    第一次提交

---
+++ .vuepress/config.js
@@ -0,0 +1,121 @@
+module.exports = {
+  "title": "XiolBlog",
+  "description": "一名前端er的博客",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  plugins: [
+    [
+      '@vuepress-reco/vuepress-plugin-kan-ban-niang',
+      {
+        theme: ['z16', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'miku'],
+        messages: {
+          welcome: '欢迎来到 ' + 'XiolBlog',
+          home: '心里的花，我想要带你回家。',
+          theme: '好吧，希望你能喜欢我的其他小伙伴。',
+          close: '你知道我喜欢吃什么吗？痴痴地望着你。'
+        },
+      }
+    ],
+    // ['vuepress-plugin-sakura']
+  ],
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "笔记",
+        "link": "https://xiol-liang.github.io/xiolblog/",
+        "icon": "reco-blog"
+      },
+      {
+        "text": "文章",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "xiol",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
