2d8ed7cf3412785e56a204820ba8e8bcb84ad7d1
xiol-liang <2318751031@qq.com>
2022-04-10 03:43:36 +0800

    增加了一篇学习博客

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd .vuepress/dist
 
 git init
 git add -A
-git commit -m '测试播放器'
+git commit -m '增加播放器插件'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
