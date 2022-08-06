dd9cbaedfb67257b9451beace18e1fbae2e8829b
xiol-liang <2318751031@qq.com>
2022-08-06 20:49:50 +0800

    增加一篇博客

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd .vuepress/dist
 
 git init
 git add -A
-git commit -m '增加了一篇博客'
+git commit -m '增加了 一篇职业生涯记录'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
